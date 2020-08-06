fce0b8d45c5306a31b20e6e193fe56739a085eec
Alan Eicker <alaneicker@gmail.com>
2020-08-06 13:23:13 -0500

    Adds mongoose and mongodb connection

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,26 @@
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
-const port = process.env.port || 9000;
+(async () => {
+  dotenv.config();
 
-app.use(bodyParser.json());
-app.use(compression());
-app.use(cors());
+  await mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+  });
 
-app.listen(port, () => {
-  console.log('Server running on port:', port);
-});
+  const db = mongoose.connection;
+  const app = express();
+  const port = process.env.port || 9000;
+
+  app.use(bodyParser.json());
+  app.use(compression());
+  app.use(cors());
+
+  app.listen(port, () => {
+    console.log('Server running on port:', port);
+  });
+})();
