ab6d31fa782f772f881b51347bd087f8e55a190d
Alan Eicker <alaneicker@gmail.com>
2020-08-11 16:20:49 -0500

    expense group detail header styles

--- src/components/expense-group-detail/ExpenseGroupDetail.js
+++ src/components/expense-group-detail/ExpenseGroupDetail.js
@@ -32,10 +32,22 @@ const ExpenseGroupDetail = () => {
   return selectedExpense ? (
     <div className="expense-group-detail">
       <div className="expense-group-detail__hd">
-        <h1 className="text-size-24 text-size-36@medium">{title}</h1>
-        <h2 className="text-size-20@medium">
-          {moment(startDate).format('L')} - {moment(endDate).format('L')}
-        </h2>
+        <div>
+          <h1 className="text-size-24 text-size-36@medium text-weight-bold">
+            {title}
+          </h1>
+          <h2 className="text-size-20">
+            {moment(startDate).format('L')} - {moment(endDate).format('L')}
+          </h2>
+        </div>
+        <div className="text-align-right@medium">
+          <h4 className="text-size-20@medium text-weight-bold">
+            Budget Amount
+          </h4>
+          <span className="text-size-20 text-size-30@medium">
+            ${budgetAmount.toLocaleString()}
+          </span>
+        </div>
       </div>
       <div className="expense-group-detail__bd">
         <ExpenseGroupForm expenses={expenses} />
