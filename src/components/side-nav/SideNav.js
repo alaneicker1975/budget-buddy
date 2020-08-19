939654665d787f92adc7db0fe46de215cec3e524
Alan Eicker <alaneicker@gmail.com>
2020-08-19 11:24:36 -0500

    Updates SideNav with "Add New Group" button

--- src/components/side-nav/SideNav.js
+++ src/components/side-nav/SideNav.js
@@ -3,26 +3,29 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import moment from 'moment';
-import { List, ListItem } from '@alaneicker/atomik-ui';
+import { List, ListItem, Button } from '@alaneicker/atomik-ui';
 
 const SideNav = ({ data, selectedExpenseId }) => {
   return (
-    <List className="side-nav">
-      {data.map(({ _id, startDate, endDate }) => {
-        return (
-          <ListItem key={_id} className="side-nav__item">
-            <Link
-              to={`/expense-group/${_id}`}
-              className={classnames({
-                'is-active': _id === selectedExpenseId,
-              })}
-            >
-              {moment(startDate).format('L')} - {moment(endDate).format('L')}
-            </Link>
-          </ListItem>
-        );
-      })}
-    </List>
+    <div className="side-nav">
+      <Button>+ New Group</Button>
+      <List className="side-nav__menu">
+        {data.map(({ _id, startDate, endDate }) => {
+          return (
+            <ListItem key={_id} className="side-nav__menu__item">
+              <Link
+                to={`/expense-group/${_id}`}
+                className={classnames({
+                  'is-active': _id === selectedExpenseId,
+                })}
+              >
+                {moment(startDate).format('L')} - {moment(endDate).format('L')}
+              </Link>
+            </ListItem>
+          );
+        })}
+      </List>
+    </div>
   );
 };
 
